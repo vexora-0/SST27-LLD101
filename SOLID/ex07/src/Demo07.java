5d75be23c4ee7b78617690b8d45abb31a42826a3
Bhargav <bhargav4g13132@gmail.com>
2025-08-25 12:16:38 +0530

    ex7

--- SOLID/ex07/src/Demo07.java
+++ SOLID/ex07/src/Demo07.java
@@ -2,6 +2,5 @@ public class Demo07 {
     public static void main(String[] args) {
         Machine m = new BasicPrinter();
         m.print("Hello");
-        m.scan("/tmp/out"); // blows up
     }
 }
---
+++ SOLID/ex07/src/IFax.java
@@ -0,0 +1,3 @@
+public interface IFax {
+    void fax(String number);
+}
