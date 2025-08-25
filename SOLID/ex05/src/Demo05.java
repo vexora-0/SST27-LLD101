125588fc4c6f6bb13332bda70d0b7ee3e352b2c7
Bhargav <bhargav4g13132@gmail.com>
2025-08-25 12:31:21 +0530

    ex 5 complete

--- SOLID/ex05/src/Demo05.java
+++ SOLID/ex05/src/Demo05.java
@@ -5,7 +5,6 @@ public class Demo05 {
         r.setHeight(4);
         return r.area();
     }
-
     public static void main(String[] args) {
         System.out.println(areaAfterResize(new Rectangle())); // 20
         System.out.println(areaAfterResize(new Square())); // 16 (!) violates expectation
